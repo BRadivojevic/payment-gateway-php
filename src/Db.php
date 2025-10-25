7d854210b4f85bd89eb6c738608eadbab3711095
Бошко Радивојевић <131773254+BRadivojevic@users.noreply.github.com>
2025-10-25 17:37:34 +0200

    Delete src directory

--- src/Db.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Db {
-  public static function conn(): PDO {
-    $dsn = getenv('DB_DSN') ?: 'sqlsrv:Server=localhost;Database=app';
-    $user = getenv('DB_USER') ?: '';
-    $pass = getenv('DB_PASS') ?: '';
-    return new PDO($dsn, $user, $pass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-  }
-}
